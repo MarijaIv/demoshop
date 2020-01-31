841b07c465d277c4a3dfab29365e6ec432596198
marija <marija.ivic@logeecom.com>
2020-01-31 14:03:44 +0100

    Eloquent ORM setup, create config.php file, create config.example.php file, describe config variables in readme.md, test database connection in index.php
    ISSUE DEMOMI-13

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 
-require_once __DIR__ . '/../composer_vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../bootstrap.php';
 
-echo 'Hello world message';
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+
+$result = Capsule::table('admin')->where('id', '=', 2)->get('username');
+echo $result;
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 This is my demo project.
+
+config.php variables description:
+
+|CONFIG VARIABLE NAME|CONFIG VARIABLE VALUE|
+|--------------------|---------------------|
+| DATABASE           | demo_database       |
+| USERNAME           | demo_username       |
+| PASSWORD           | demo_password       |
